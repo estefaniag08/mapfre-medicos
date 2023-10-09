9a8d6c20d70db17f1482ff6c0eb2b3edae5808f0
Cristian <cristiangaitanmora@gmail.com>
2023-10-09 16:38:51 -0500

    Corregido el formulario de paso-contacto, agregados los campos de "Celular", "Correo" y "Confirmar Correo".

--- src/app/formularios-medicos/formulario-cotizacion/paso-contacto/paso-contacto.component.ts
+++ src/app/formularios-medicos/formulario-cotizacion/paso-contacto/paso-contacto.component.ts
@@ -17,7 +17,7 @@ import {
 })
 export class PasoContactoComponent implements OnInit {
   sobreTi: FormGroup;
-
+  contacto: FormGroup;
   /**
    * @description Variable que permite definir la fecha mínima para iniciar la selección en el calendario
    * @type Date
@@ -28,6 +28,14 @@ export class PasoContactoComponent implements OnInit {
    * @type Date
    */
   maxDate: Date;
+
+  /**
+   * @description Variable que identifica de cara al ciudadano si el valor que intenta ingresar en alfabético
+   */
+  esLetra: boolean;
+  mobilePhonePattern = "^(?=.*[0-9]).{7,10}$";
+  // emailPattern ="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.a-zA-Z]$"
+  emailPattern: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   /**
    *
@@ -43,11 +51,15 @@ export class PasoContactoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.sobreTi = this.formBuilder.group({
+    this.contacto = this.formBuilder.group({
       nombresMedico: ['', Validators.required],
       apellidosMedico: ['', Validators.required],
-      fechaNacimiento: ['', Validators.required],
-      genero: ['', Validators.required],
+      numeroCelular: ['', Validators.compose([  Validators.required, Validators.pattern(this.mobilePhonePattern)]),],
+      correo: ['', Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])],
+      confirmacionCorreo: [
+        '',
+        Validators.compose([Validators.required, Validators.pattern(this.emailPattern)]),
+      ],
     });
   }
 
@@ -66,6 +78,31 @@ export class PasoContactoComponent implements OnInit {
       this.personaSolicServ.anadirPersona(infoPersona);
     }
   }
+  
+  //Sin descripción
+  errorCelular() {
+    return 'Ingresa tu número de celular para continuar';
+  }
+
+  //Sin descripción
+  errorCorreo() {
+    if (this.contacto.controls['correo'].hasError('required')) {
+      return 'Ingresa un correo para continuar';
+    }
+    if(this.contacto.controls['correo'].hasError('pattern')) {
+      return "Ingresa un correo válido para continuar"
+    }
+  }
+
+  //Sin descripción
+  errorConfirmacionCorreo() {
+    if (this.contacto.controls['confirmacionCorreo'].hasError('required')) {
+      return 'Ingresa un correo para continuar';
+    }
+    if(this.contacto.controls['confirmacionCorreo'].hasError('pattern')) {
+      return "Ingresa un correo válido para continuar"
+    }
+  }
 
   /**
    * @method
@@ -81,7 +118,49 @@ export class PasoContactoComponent implements OnInit {
     return 'Ingresa tu apellido para continuar';
   }
 
-  errorFecha() {
-    return 'Selecciona una fecha para continuar';
+  /**
+   * @method
+   * @description Bloquea las letras en los campos que solo pueden recibir números del formulario
+   * @param key Permite capturar el código ASCII de la tecla presionada por el usuario
+   * CÓDIGOS ASCCII DE TECLAS A TENER EN CUENTA
+   * Código 48 al 57 && 96 al 105: Corresponden a los números del 0 al 9
+   * Código 8: Corresponde al backspace o borrar
+   * Código 39: Corresponde a la tecla de flecha izquierda
+   * Código 37: Corresponde a la tecla de flecha derecha
+   * Código 9: Corresponde a la tecla tab
+   */
+  bloquearLetras(key) {
+    let char = key.keyCode;
+    if (
+      !(
+        (char > 47 && char < 58) ||
+        (char > 94 && char < 106) ||
+        char == 8 ||
+        char == 39 ||
+        char == 37 ||
+        char == 9
+      )
+    ) {
+      key.preventDefault();
+      this.esLetra = false;
+    }
+    this.esLetra = true;
+  }
+
+  /**
+   * @method
+   * @description Permite validar que el correo ingresado en el campo de correo y confirmación de correo sean el mismo
+   */
+  validarConfirmacionCorreo() {
+    if (
+      this.contacto.controls['correo'].touched &&
+      this.contacto.controls['confirmacionCorreo'].touched
+    ) {
+      if (
+        this.contacto.value.correo !=
+        this.contacto.value.confirmacionCorreo
+      )
+        return 'Los valores deben coincidir';
+    }
   }
 }
